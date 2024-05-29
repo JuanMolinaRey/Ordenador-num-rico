6a7921fe49f7d21117b2d0e2fe4f88c0a1a4a01c
Juan Antonio Molina Rey <JuanMolinaRey7@gmail.com>
2024-05-29 08:46:04 +0200

    final

--- src/main/java/factoria/juanantonio/App.java
+++ src/main/java/factoria/juanantonio/App.java
@@ -1,43 +1,56 @@
 package factoria.juanantonio;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class App {
-    
-    
-        public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-            
 
-        System.out.println("~~~~~~~~Ordenador de Números.~~~~~~~~~~");
+    public static void main(String[] args) {
+        Scanner entradaDatos = new Scanner(System.in);
+        ArrayList<Integer> numeros = new ArrayList<>();
 
-        System.out.println("-Introduzca la cantidad de números a ordenar: ");
- 
-        int cantidadNumeros= scanner.nextInt();
+        System.out.println("Introduzca los números seguidos de un enter e introduzca 'fin' para terminar):");
 
-        int numeros [] = {cantidadNumeros};
-        
-         
-        Arrays.sort(numeros);
-
-        String A = "Ascendente";
-        String D = "Descendente";
-
-        System.out.println("¿De que forma quieres que se coloquen los números? " +A+ " o " +D+" : ");
-
-        //if () {
-
-         for (int i = 0; i < numeros.length; i++) {
-         System.out.println(""+numeros[i]);
-
+        while (true) {
+            String input = entradaDatos.nextLine();
+            if (input.equalsIgnoreCase("fin")) {
+                break;
+            }
+            try {
+                int numero = Integer.parseInt(input);
+                numeros.add(numero);
+            } catch (NumberFormatException e) {
+                System.out.println("Por favor, introduzca un número válido o 'fin' para terminar.");
+            }
         }
-         for (int i = numeros.length-1; i >= 0; i--) {
-            System.out.println(""+numeros[i]);
-   
-           }
-//}
 
-     }
- }
-    
+        if (numeros.isEmpty()) {
+            System.out.println("No se han introducido números.");
+            entradaDatos.close();
+            return;
+        }
+
+        System.out.println("¿Cómo quieres ordenar la cadena numérica? (ascendente/descendente)");
+        String decision = entradaDatos.nextLine();
+
+        if (decision.equalsIgnoreCase("ascendente")) {
+            Collections.sort(numeros);
+            System.out.println("Tus números ordenados ascendent1emente son:");
+            for (int numero : numeros) {
+                System.out.println(numero);
+            }
+        } else if (decision.equalsIgnoreCase("descendente")) {
+            Collections.sort(numeros, Collections.reverseOrder());
+            System.out.println("Tus números ordenados descendentemente son:");
+            for (int numero : numeros) {
+                System.out.println(numero);
+            }
+        }
+        else{
+            System.out.println("Opción no válida. Por favor, elija 'ascendente' o 'descendente'.");
+        }
+
+        entradaDatos.close();
+    }
+}
